e1a31a21ce17f485ab803df533c88becb338e0a0
Gonzalo Mendoza <yogonza524@gmail.com>
2017-05-05 17:34:21 -0300

    f

---
+++ src/main/java/com/gaming/ejemplo/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gaming.ejemplo;
+
+/**
+ *
+ * @author Gonzalo H. Mendoza
+ * Web: http://idsoft.com.ar
+ * Mail: yogonza524@gmail.com
+ * StackOverflow: http://stackoverflow.com/users/5079517/gonza?tab=profile
+ */
+public class Main {
+    
+    // <editor-fold defaultstate="collapsed" desc="Atributos">
+
+    //</editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Constructor">
+
+    //</editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Getters y Setters">
+
+    //</editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Metodos">
+    public static void main(String[] args) {
+        System.out.println("Ejemplo");
+    }
+    //</editor-fold>
+}
